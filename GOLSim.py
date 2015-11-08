801dffbd4e562fa1cef786906ca49ad612445d60
Dominik Ivošević <dominik.dito@gmail.com>
2015-11-08 20:46:38 +0100

    Started writing server

---
+++ GOLSim.py
@@ -0,0 +1,14 @@
+import fileimput
+import random
+import json
+from State import State
+
+if __name__ == '__main__':
+  state = State()
+
+  for line in fileinput.input(bufsize=1):
+    changes = json.loads(line)['cells']
+    for change in changes:
+      state.change(change)
+
+    print(state.getJSON())
