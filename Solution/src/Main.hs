dfae30239b1693cbcdcb52f96f6562771752984a
Luka Horvat <lukahorvat9@gmail.com>
2015-11-09 00:35:03 +0100

    Slow

--- Solution/src/Main.hs
+++ Solution/src/Main.hs
@@ -7,6 +7,8 @@ import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Lazy.Char8 as LazyBS
 import Control.Monad
 import System.IO (hFlush, stdout)
+import Strategy (Moves(..), GameState(GameState), CellState(..), decide)
+import qualified Data.Vector as Vec
 
 data ServerInfo = ServerInfo { field                   :: [String]
                              , cellsRemaining          :: Int
@@ -30,8 +32,22 @@ leftest :: ServerInfo -> (Int, Int)
 leftest ServerInfo{..} = maximum [(x, y) | x <- [0..23], y <- [0..23], field !! y !! x == '#']
 
 makeMove :: ServerInfo -> Move
-makeMove si = Move [(y, x + 1)]
-    where (x, y) = leftest si
+makeMove = movesToMove . decide . serverInfoToGameState
+
+movesToMove :: Moves -> Move
+movesToMove (Moves mvs) = Move $ map (\(x, y) -> (y, x)) mvs
+
+charToCell :: Char -> CellState
+charToCell '.' = Empty
+charToCell '#' = Ally
+charToCell 'O' = Enemy
+charToCell _   = error "Bad cell"
+
+serverInfoToGameState :: ServerInfo -> GameState
+serverInfoToGameState ServerInfo{..} = GameState (Vec.fromList $ map charToCell $ concat field) 0
+
+logFile :: Show a => a -> IO ()
+logFile x = appendFile "log.txt" (show x)
 
 main :: IO ()
 main = forever $ do
@@ -39,6 +55,7 @@ main = forever $ do
     case parseRes of
         Just servInfo -> do
             let move = Aes.encode $ makeMove servInfo
+            logFile move
             LazyBS.putStr move
             putStr "\n"
             hFlush stdout
--- Solution/src/Strategy.hs
+++ Solution/src/Strategy.hs
@@ -6,6 +6,9 @@ import qualified Data.Vector as Vec
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Int
+import Data.Ord
+import Data.List
+import System.IO.Unsafe
 
 newtype Moves = Moves [(Int, Int)]
 
@@ -18,22 +21,46 @@ data GameState = GameState { field :: Vector CellState
 gridHeight, gridWidth :: Int
 (gridHeight, gridWidth) = (24, 24)
 
+gridIdx :: [(Int, Int)]
+gridIdx = [(x, y) | x <- [0..gridWidth - 1], y <- [0..gridHeight - 1]]
+
+oneOffset :: [(Int, Int)]
+oneOffset = [(x, y) | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0]
+
+twoOffset :: [(Int, Int)]
+twoOffset = [(x, y) | x <- [-2..2], y <- [-2..2], x /= 0 || y /= 0]
+
 at :: GameState -> (Int, Int) -> CellState
-(st@GameState{..}) `at` (x, y)
-    |    x >= 0 && x < gridWidth
-      && y >= 0 && y < gridHeight = field Vec.! (x + y * gridHeight)
-    | otherwise                   = st `at` (x `mod` gridWidth, y `mod` gridHeight)
+(st@GameState{..}) `at` (x, y) = field Vec.! idx (x, y)
 
 coords :: Int -> (Int, Int)
 coords n = let (d, r) = n `divMod` gridWidth in (r, d)
 
+idx :: (Int, Int) -> Int
+idx (x, y)
+    |    x >= 0 && x < gridWidth
+      && y >= 0 && y < gridHeight = x + y * gridWidth
+    | otherwise                   = idx (x `mod` gridWidth, y `mod` gridHeight)
+
+numCell :: CellState -> Int
+numCell Empty = 0
+numCell Ally  = 1
+numCell Enemy = -1
+
 neighbors :: GameState -> (Int, Int) -> Int
-neighbors = undefined
+neighbors gs (x, y) = s
+    where s = sum $ map (\(i, j) -> numCell $ gs `at` (x + i, y + j)) oneOffset
+
+legal :: GameState -> (Int, Int) -> Bool
+legal gs (x, y) = gs `at` (x, y) == Empty && close
+    where all2Neigh = map (\(i, j) -> gs `at` (x + i, y + j)) oneOffset
+          close     = Ally `elem` all2Neigh
 
 logic :: (Int, Int) -> CellState -> GameState -> CellState
 logic (x, y) st gs = case st of
     Empty | ns == 3            -> Ally
           | ns == -3           -> Enemy
+          | otherwise          -> Empty
     Ally  | ns `elem` [2, 3]   -> Ally
           | otherwise          -> Empty
     Enemy | ns `elem` [-2, -3] -> Enemy
@@ -44,11 +71,25 @@ advance :: GameState -> GameState
 advance gs = gs { field = Vec.imap (\i st -> logic (coords i) st gs) (field gs) }
 
 allMoves :: GameState -> Moves
-allMoves = undefined
+allMoves gs = Moves $ filter (legal gs) gridIdx
 
-kCombinations :: Moves -> [Moves]
-kCombinations = undefined
+subsequencesOfSize :: Int -> [a] -> [[a]]
+subsequencesOfSize n xs = let l = length xs
+                          in if n>l then [] else subsequencesBySize xs !! (l-n)
+    where subsequencesBySize [] = [[[]]]
+          subsequencesBySize (x:xs) = let next = subsequencesBySize xs
+                                      in zipWith (++) ([]:next) (map (map (x:)) next ++ [[]])
+
+kCombinations :: Int -> Moves -> [Moves]
+kCombinations k (Moves mvs) = map Moves $ subsequencesOfSize k mvs
+
+evaluate :: GameState -> Int
+evaluate gs = sum $ map (numCell . (gs `at`)) gridIdx
+
+goodness :: GameState -> Moves -> Int
+goodness (gs@GameState{..}) (Moves mvs) = evaluate $ advance $ gs { field = newField }
+    where newField = field Vec.// map (\(x, y) -> (idx (x, y), Ally)) mvs
 
 decide :: GameState -> Moves
-decide gs
-    where allMoves
+decide gs = maximumBy (comparing (goodness gs)) allCombos
+    where allCombos = kCombinations 2 $ allMoves gs
